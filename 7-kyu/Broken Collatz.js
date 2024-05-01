b56a379fa8a7df97d4cbda3317dd295876c72d06
unknown <ismailovsardorbek792@gmail.com>
2024-05-01 12:34:05 +0500

    kata solution

--- 7-kyu/Broken Collatz.js
+++ 7-kyu/Broken Collatz.js
@@ -1,6 +1,8 @@
-const sumSquareEvenRootOdd = (ns) => {
-  return +ns
-    .map((n) => (n % 2 ? Math.sqrt(n) : n ** 2))
-    .reduce((acc, cur) => acc + cur, 0)
-    .toFixed(2)
+function collatz(n, count = 1) {
+  while (n != 1) {
+    count++
+    n = n % 2 ? n * 3 + 1 : n / 2
+  }
+
+  return count
 }
